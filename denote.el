27642bee140263fc5a3749c1e2be886013e538e4
Protesilaos Stavrou <info@protesilaos.com>
2022-06-05 13:44:03 +0300

    Clarify 'denote' doc string

--- denote.el
+++ denote.el
@@ -295,7 +295,7 @@ Completion candidates are those of `denote-known-keywords'.  If
 file names are also provided as candidates.
 
 When `denote-sort-keywords' is non-nil, keywords are sorted
-alphabetically."
+alphabetically in both the file name and file contents."
   (declare (interactive-only t))
   (interactive
    (list
