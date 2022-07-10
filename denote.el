a283cc1139c2aee7417fbddc501340245ad0b2bf
Protesilaos Stavrou <info@protesilaos.com>
2022-07-10 11:01:26 +0300

    Revert "Avoid errors with string value for keywords"
    This reverts commit 92e15886d51925711b64042b772078809d4828d6.
    
    That is not the right approach, as it can cause problems elsewhere.

--- denote.el
+++ denote.el
@@ -370,11 +370,10 @@ trailing hyphen."
 
 (defun denote--sluggify-keywords (keywords)
   "Sluggify KEYWORDS."
-  (let ((kws (if (listp keywords) keywords (list keywords))))
-    (mapcar (if denote-allow-multi-word-keywords
-                #'denote--sluggify
-              #'denote--sluggify-and-join)
-            kws)))
+  (mapcar (if denote-allow-multi-word-keywords
+              #'denote--sluggify
+            #'denote--sluggify-and-join)
+          keywords))
 
 (defun denote--file-empty-p (file)
   "Return non-nil if FILE is empty."
