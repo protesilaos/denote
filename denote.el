8103476e311983972b65574d81920ae63692a3e6
Protesilaos Stavrou <info@protesilaos.com>
2022-08-10 08:11:04 +0300

    Simplify docstrings of helper date functions

--- denote.el
+++ denote.el
@@ -753,20 +753,17 @@ provided by `denote'. FILETYPE is one of the values of
 ;; Adapted from `org-hugo--org-date-time-to-rfc3339' in the `ox-hugo'
 ;; package: <https://github.com/kaushalmodi/ox-hugo>.
 (defun denote--date-rfc3339 (date)
-  "Format date using the RFC3339 specification.
-If DATE is nil, use the current date."
+  "Format date using the RFC3339 specification."
   (replace-regexp-in-string
    "\\([0-9]\\{2\\}\\)\\([0-9]\\{2\\}\\)\\'" "\\1:\\2"
    (format-time-string "%FT%T%z" date)))
 
 (defun denote--date-org-timestamp (date)
-  "Format date using the Org inactive timestamp notation.
-If DATE is nil, use the current date."
+  "Format date using the Org inactive timestamp notation."
   (format-time-string "[%F %a %R]" date))
 
 (defun denote--date-iso-8601 (date)
-  "Format date according to ISO 8601 standard.
-If DATE is nil, use the current date."
+  "Format date according to ISO 8601 standard."
   (format-time-string "%F" date))
 
 (defun denote--date (date file-type)
