459294c84b20e6670dfc943b159df1a88b7ea08c
Protesilaos Stavrou <info@protesilaos.com>
2022-09-14 07:53:58 +0300

    Refine doc string of denote-directory-files

--- denote.el
+++ denote.el
@@ -501,7 +501,9 @@ FILE must be an absolute path."
 (defun denote-directory-files ()
   "List absolute file paths in variable `denote-directory'.
 The returned files only need to have an identifier.  This may
-include files that are not implied by `denote-file-types'."
+include files that are not implied by `denote-file-types'.
+Remember that the variable `denote-directory' accepts a dir-local
+value, as explained in its doc string."
   (mapcar
    #'expand-file-name
    (seq-remove
