234520b9d7aac866d0be32c3bb9aaa3a04dd5a00
Florian <florian.denote@aboulafia.org>
2022-10-18 14:17:57 +0200

    Fix file renaming when using version control
    If dired-vc-rename-file is set to t, renaming a file under version
    control with Denote in a Dired buffer will rename the file using
    version control (e.g. "git mv")

--- denote.el
+++ denote.el
@@ -1697,7 +1697,9 @@ the file type is assumed to be the first of `denote-file-types'."
 (defun denote-rename-file-and-buffer (old-name new-name)
   "Rename file named OLD-NAME to NEW-NAME, updating buffer name."
   (unless (string= (expand-file-name old-name) (expand-file-name new-name))
-    (rename-file old-name new-name nil)
+    (if (derived-mode-p 'dired-mode)
+        (dired-rename-file old-name new-name nil)
+      (rename-file old-name new-name nil))
     (denote--rename-buffer old-name new-name)))
 
 (define-obsolete-function-alias
