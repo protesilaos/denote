protesilaos/denote

commit 2e444f3f224797131506ac81c0cc62728c16af0e

BREAKING make 'denote-file-prompt' accept optional regexp filter

denote.el
@@ -888,14 +888,17 @@ The path is relative to DIRECTORY (default: ‘default-directory’)."
 (defvar denote--file-history nil
   "Minibuffer history of `denote-file-prompt'.")
 
-(defun denote-file-prompt (&optional initial-text)
+(defun denote-file-prompt (&optional files-matching-regexp)
   "Prompt for file with identifier in variable `denote-directory'.
-With optional INITIAL-TEXT, use it to prepopulate the minibuffer."
-  (let* ((all-files (denote-all-files))
+With optional FILES-MATCHING-REGEXP, filter the candidates per
+the given regular expression."
+  (let* ((files (if files-matching-regexp
+                    (denote-directory-files-matching-regexp files-matching-regexp)
+                  (denote-all-files)))
          (completion-ignore-case read-file-name-completion-ignore-case))
-    (when all-files
+    (when files
       (funcall project-read-file-name-function
-               "Select note: " all-files nil 'denote--file-history initial-text))))
+               "Select note: " files nil 'denote--file-history))))
 
 (define-obsolete-function-alias
   'denote--retrieve-read-file-prompt

README.org
@@ -3662,8 +3662,10 @@ might change them without further notice.
 
 #+findex: denote-file-prompt
 + Function ~denote-file-prompt~ :: Prompt for file with identifier in
-  variable ~denote-directory~.  With optional =INITIAL-TEXT=, use it
-  to prepopulate the minibuffer.
+  variable ~denote-directory~.  With optional =FILES-MATCHING-REGEXP=,
+  filter the candidates per the given regular expression. [ Refactored
+  as part of {{{development-version}}} because the old =INITIAL-INPUT=
+  argument was not actually used anywhere. ]
 
 #+findex: denote-keywords-prompt
 + Function ~denote-keywords-prompt~ :: Prompt for one or more

commit 86f91ea3fa636c9fc30952d1fd79dbdcdb52ed0f

Fix regression with how 'denote-link' was called
These were caused by commit 2e444f3.

denote.el
@@ -3169,7 +3169,12 @@ file."
       (call-interactively #'denote)
       (save-buffer)
       (setq path (buffer-file-name)))
-    (denote-link path id-only)))
+    (let ((type (denote-filetype-heuristics path)))
+      (denote-link
+       path
+       type
+       (denote--link-get-description path type)
+       id-only))))
 
 ;;;###autoload
 (defun denote-link-after-creating-with-command (command &optional id-only)
@@ -3188,7 +3193,14 @@ Optional ID-ONLY has the same meaning as in the command
       (call-interactively command)
       (save-buffer)
       (setq path (buffer-file-name)))
-    (denote-link path id-only)))
+    (let ((type (denote-filetype-heuristics path)))
+      (denote-link
+       path
+       type
+       (if (eq command 'denote-signature)
+           (denote--link-get-description-with-signature path type)
+         (denote--link-get-description path type))
+       id-only))))
 
 ;;;###autoload
 (defun denote-link-or-create (target &optional id-only)
@@ -3210,7 +3222,7 @@ consists of just the identifier.  Else try to also include the
 file's title.  This has the same meaning as in `denote-link'."
   (interactive (list (denote-file-prompt) current-prefix-arg))
   (if (and target (file-exists-p target))
-      (denote-link target id-only)
+      (denote-link target (denote-filetype-heuristics target) target id-only)
     (denote--command-with-title-history #'denote-link-after-creating)))
 
 (defalias 'denote-link-to-existing-or-new-note 'denote-link-or-create