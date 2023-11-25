6d26aea464a234cdfce4b76dd3a1ecfd1f562ab8
Protesilaos Stavrou <info@protesilaos.com>
2023-11-25 05:51:40 +0200

    Remove :reverse parameter from 'denote-backlinks' Org dynamic block
    [ Same idea as with commit 0063240. ]
    
    We do not expose this feature to the commands we provide. Adding it
    here creates inconsistencies, such as, for example, with the
    'denote-files' Org dynamic block. I prefer to remove this option than
    to see its proliferation.

--- denote-org-dblock.el
+++ denote-org-dblock.el
@@ -72,7 +72,7 @@ Used by `org-dblock-update' with PARAMS provided by the dynamic block."
 (defun denote-org-dblock-insert-backlinks ()
   "Insert new Org dynamic block to include backlinks."
   (interactive)
-  (org-create-dblock (list :name "denote-backlinks" :reverse nil))
+  (org-create-dblock (list :name "denote-backlinks"))
   (org-update-dblock))
 
 (org-dynamic-block-define "denote-backlinks" 'denote-org-dblock-insert-backlinks)
@@ -83,9 +83,8 @@ Used by `org-dblock-update' with PARAMS provided by the dynamic block."
   (when-let ((file (buffer-file-name))
              (id (denote-retrieve-filename-identifier file))
              (files (delete file (denote--retrieve-files-in-xrefs id))))
-    (let ((denote-link-add-links-sort (plist-get params :reverse)))
-      (insert (denote-link--prepare-links files file nil))
-      (join-line)))) ;; remove trailing empty line
+    (insert (denote-link--prepare-links files file nil))
+    (join-line))) ; remove trailing empty line
 
 ;;;; Dynamic block to insert entire file contents
 
