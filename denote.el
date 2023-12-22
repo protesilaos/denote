7c79e89d1f68c0b4f55db67f999a0b55b0c2e912
Protesilaos Stavrou <info@protesilaos.com>
2023-12-22 05:56:29 +0200

    Make denote--edit-front-matter-p check if front matter is non-nil
    We do not want to try to rewrite front matter if the given file type has
    a nil value for its front matter. Furthermore, we do not want to make
    the inclusion of front matter mandatory. Front matter is not a
    requirement for Denote, as the file name is our point of reference.
    
    Thanks to Glenna D. for reporting an error produced by
    'denote-dired-rename-marked-files-with-keywords' in a scenario where
    the front matter would be nil. This was done via a private channel
    and the information is shared with permission.

--- denote.el
+++ denote.el
@@ -2307,7 +2307,8 @@ in a Denote note.
 
 For the purposes of this test, FILE is a Denote note when it
 contains a title line, a keywords line or both."
-  (and (denote--regexp-in-file-p (denote--title-key-regexp file-type) file)
+  (and (denote--front-matter file-type)
+       (denote--regexp-in-file-p (denote--title-key-regexp file-type) file)
        (denote--regexp-in-file-p (denote--keywords-key-regexp file-type) file)))
 
 (defun denote-rewrite-keywords (file keywords file-type)
