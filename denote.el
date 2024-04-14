417818b3319751aaf6166f3ef7ddeedf3b769a4b
Jean-Philippe Gagné Guay <jeanphilippe150@gmail.com>
2024-04-14 15:26:43 -0400

    Remove denote-retrieve-front-matter-keywords-value-as-string

--- denote.el
+++ denote.el
@@ -1733,21 +1733,11 @@ Subroutine of `denote--file-with-temp-buffer'."
 
 (defun denote-retrieve-front-matter-keywords-value (file file-type)
   "Return keywords value from FILE front matter per FILE-TYPE.
-The return value is a list of strings.  To get a combined string
-the way it would appear in a Denote file name, use
-`denote-retrieve-front-matter-keywords-value-as-string'."
+The return value is a list of strings."
   (denote--file-with-temp-buffer file
     (when (re-search-forward (denote--keywords-key-regexp file-type) nil t 1)
       (funcall (denote--keywords-value-reverse-function file-type)
                (buffer-substring-no-properties (point) (line-end-position))))))
-
-(defun denote-retrieve-front-matter-keywords-value-as-string (file file-type)
-  "Return keywords value from FILE front matter per FILE-TYPE.
-The return value is a string, with the underscrore as a separator
-between individual keywords.  To get a list of strings instead,
-use `denote-retrieve-front-matter-keywords-value' (the current function uses
-that internally)."
-  (denote-keywords-combine (denote-retrieve-front-matter-keywords-value file file-type)))
 
 (defun denote-retrieve-front-matter-keywords-line (file file-type)
   "Return keywords line from FILE front matter per FILE-TYPE."
@@ -1766,9 +1756,6 @@ that internally)."
 
 (defalias 'denote-retrieve-keywords-line 'denote-retrieve-front-matter-keywords-line
  "Alias for `denote-retrieve-front-matter-keywords-line'.")
-
-(defalias 'denote-retrieve-keywords-value-as-string 'denote-retrieve-front-matter-keywords-value-as-string
- "Alias for `denote-retrieve-front-matter-keywords-value-as-string'.")
 
 (define-obsolete-function-alias
   'denote--retrieve-title-or-filename
