protesilaos/denote

commit f08bed090985d00aa99f12cf1a255a23fd265f68

Make denote-retrieve-title-or-filename be a wrapper only
We do not want it to return the file-name-base because this will
duplicate the text of the file name when there is no TITLE component,
as demonstrated by duli in issue 347: <https://github.com/protesilaos/denote/issues/347>.

denote.el
@@ -1796,17 +1796,13 @@ The return value is a list of strings."
 
 (defun denote-retrieve-title-or-filename (file type)
   "Return appropriate title for FILE given its TYPE.
-Try to find the value of the title in the front matter of FILE,
-otherwise use its file name.
-
 This is a wrapper for `denote-retrieve-front-matter-title-value' and
 `denote-retrieve-filename-title'."
   (if-let (((denote-file-is-note-p file))
            (title (denote-retrieve-front-matter-title-value file type))
            ((not (string-blank-p title))))
       title
-    (or (denote-retrieve-filename-title file)
-        (file-name-base file))))
+    (denote-retrieve-filename-title file)))
 
 (defun denote--retrieve-location-in-xrefs (identifier)
   "Return list of xrefs for IDENTIFIER with their respective location.

README.org
@@ -4360,10 +4360,11 @@ might change them without further notice.
 
 #+findex: denote-retrieve-title-or-filename
 + Function ~denote-retrieve-title-or-filename~ :: Return appropriate
-  title for =FILE= given its =TYPE=. Try to find the value of the
-  title in the front matter of FILE, otherwise use its file name. This
-  is a wrapper for ~denote-retrieve-front-matter-title-value~ and
-  =denote-retrieve-filename-title=.
+  title for =FILE= given its =TYPE=. This is a wrapper for
+  ~denote-retrieve-front-matter-title-value~ and
+  =denote-retrieve-filename-title=. [ Simplified as part of
+  {{{development-version}}} to only be a wrapper as its doc string
+  suggests. ]
 
 #+findex: denote-get-identifier
 + Function ~denote-get-identifier~ :: Convert =DATE= into a Denote
@@ -5438,7 +5439,7 @@ Denote is meant to be a collective effort.  Every bit of help matters.
   Prevos, Peter Smith, Suhail Singh, Shreyas Ragavan, Stefan Thesing,
   Summer Emacs, Sven Seebeck, Taoufik, TJ Stankus, Vick (VicZz),
   Viktor Haag, Wade Mealing, Yi Liu, Ypot, atanasj, babusri, doolio,
-  drcxd, fingerknight, hpgisler, mentalisttraceur, pRot0ta1p,
+  duli, drcxd, fingerknight, hpgisler, mentalisttraceur, pRot0ta1p,
   rbenit68, relict007, sienic, sundar bp.
 
 Special thanks to Peter Povinec who helped refine the file-naming

commit 23d268a15675cc7175ad74f463d1575b098dc24c

Fix regression caused by commit f08bed0
We want the title prompt to do the following:

1. Find the title from the file's front matter.
2. Find the title from the relevant Denote file name component.
3. Fall back to the name of the file UNLESS it already has an
   identifier. In this case, the file does not have a Denote title
   component, so we do not want to put the rest of the file name (e.g.
   identifier plus signature/keywords) as the initial title as that
   will duplicate the contents of the file name.

We had this before, but point 3 was failing.

Thanks to Alan Schmitt for bringing this matter to my attention in
discussion 353: <https://github.com/protesilaos/denote/discussions/353>.

denote.el
@@ -1795,7 +1795,9 @@ This is a wrapper for `denote-retrieve-front-matter-title-value' and
            (title (denote-retrieve-front-matter-title-value file type))
            ((not (string-blank-p title))))
       title
-    (denote-retrieve-filename-title file)))
+    (or (denote-retrieve-filename-title file)
+        (and (not (denote-file-has-identifier-p file))
+             (file-name-base file)))))
 
 (defun denote--retrieve-location-in-xrefs (identifier)
   "Return list of xrefs for IDENTIFIER with their respective location.