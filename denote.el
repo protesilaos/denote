51cb46b6b26fd63fc22cbc52baf7471ba0f22936
Protesilaos Stavrou <info@protesilaos.com>
2024-07-01 19:22:20 +0300

    Bump version to trigger GNU ELPA rebuild for commit d942333

--- denote.el
+++ denote.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/denote
-;; Version: 3.0.1
+;; Version: 3.0.2
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
