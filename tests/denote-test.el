c66521caeb0d1fc26a703151132d5747b1890d56
Protesilaos Stavrou <info@protesilaos.com>
2024-09-01 09:25:15 +0300

    Add missing year range to denote-test.el copyright header

--- tests/denote-test.el
+++ tests/denote-test.el
@@ -1,6 +1,6 @@
 ;;; denote-test.el --- Unit tests for Denote -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023  Free Software Foundation, Inc.
+;; Copyright (C) 2023-2024  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Denote Development <~protesilaos/denote@lists.sr.ht>
