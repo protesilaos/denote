protesilaos/denote

commit 23bc9dbca31c0b0d8d30b691d7bb0ba366834cb1

Define :help-echo function for Org links
Thanks to Julian Hoch for asking about this. It was done via a private
channel and I am sharing the information with permission.

denote.el
@@ -4942,6 +4942,14 @@ backend."
      ((eq format 'md) (format "[%s](%s)" desc path))
      (t path))))
 
+(defun denote-link-ol-help-echo (_window _object position)
+  "Echo the full file path of the identifier at POSITION."
+  (when-let ((htmlize-link (get-text-property position 'htmlize-link))
+             (string (plist-get htmlize-link :uri))
+             (identifier (replace-regexp-in-string "denote:\\(.*?\\)\\(#.*\\)?" "\\1" string))
+             (path (denote-get-path-by-id identifier)))
+    path))
+
 ;; The `eval-after-load' part with the quoted lambda is adapted from
 ;; Elfeed: <https://github.com/skeeto/elfeed/>.
 
@@ -4957,6 +4965,7 @@ backend."
            "denote"
            :follow #'denote-link-ol-follow
            :face 'denote-faces-link
+           :help-echo #'denote-link-ol-help-echo
            :complete #'denote-link-ol-complete
            :store #'denote-link-ol-store
            :export #'denote-link-ol-export)))))

commit ce75c1ed49b3ab26dfe900f3b48a3291478ebeff

Fix regression in denote-get-identifier-at-point caused by commit 23bc9db
Thanks to Alan Schmitt for noting that the function had stopped
working. This was done in issue 449:
<https://github.com/protesilaos/denote/issues/449>.

denote.el
@@ -4332,9 +4332,10 @@ Implementation based on the function `org-activate-links'."
              (_ (or (eq face-at-point 'denote-faces-link)
                     (member 'denote-faces-link face-at-point))))
     (or (get-text-property position 'denote-link-id)
-        (let ((property (get-text-property position 'help-echo)))
-          (string-match denote-id-regexp property)
-          (match-string-no-properties 0 property)))))
+        (when-let ((link-data (get-text-property position 'htmlize-link))
+                   (link (cadr link-data)))
+          (string-match denote-id-regexp link)
+          (match-string-no-properties 0 link)))))
 
 (defun denote--get-link-file-path-at-point (&optional point)
   "Return link to the Denote file path at point or optional POINT.