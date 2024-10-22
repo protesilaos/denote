protesilaos/denote

commit 4db0214d301c3893813b8ed55166014252895819

Do not check for nil parameter in denote--dir-in-denote-directory-p

denote.el
@@ -2046,9 +2046,7 @@ TEMPLATE, and SIGNATURE should be valid for note creation."
 
 (defun denote--dir-in-denote-directory-p (directory)
   "Return non-nil if DIRECTORY is in variable `denote-directory'."
-  (and directory
-       (string-prefix-p (denote-directory)
-                        (expand-file-name directory))))
+  (string-prefix-p (denote-directory) (expand-file-name directory)))
 
 (defun denote--valid-file-type (filetype)
   "Return a valid filetype symbol given the argument FILETYPE.

commit d99d276ebae7ff541d9ae52a029477eccdf28f1d

Fix regression caused by commit 4db0214

denote.el
@@ -2381,7 +2381,7 @@ instead of that of the parameter."
          (file-type (denote--valid-file-type (or file-type denote-file-type)))
          (keywords (denote-keywords-sort keywords))
          (date (denote-parse-date date))
-         (directory (if (denote--dir-in-denote-directory-p directory)
+         (directory (if (and directory (denote--dir-in-denote-directory-p directory))
                         (file-name-as-directory directory)
                       (denote-directory)))
          (template (if (or (stringp template) (functionp template))
@@ -2802,7 +2802,7 @@ Do it if BUF is in Dired mode and is either part of the variable
   (let ((current (current-buffer)))
     (with-current-buffer buf
       (when (and (eq major-mode 'dired-mode)
-                 (or (denote--dir-in-denote-directory-p default-directory)
+                 (or (and default-directory (denote--dir-in-denote-directory-p default-directory))
                      (eq current buf)))
         (revert-buffer)))))
 