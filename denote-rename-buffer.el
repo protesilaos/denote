614fa7e50f7d195370dc0862883a9b2078b6729d
Protesilaos Stavrou <info@protesilaos.com>
2024-10-26 06:22:19 +0300

    Fix typo in denote-rename-buffer-format doc string

--- denote-rename-buffer.el
+++ denote-rename-buffer.el
@@ -49,7 +49,7 @@
 
 (defcustom denote-rename-buffer-format "[D] %t%b"
   "The format of the buffer name `denote-rename-buffer' should use.
-Thie value is a string that treats specially the following
+The value is a string that treats specially the following
 specifiers:
 
 - The %t is the Denote TITLE of the file.
