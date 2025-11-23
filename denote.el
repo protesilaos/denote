133439ba1e273cc3e712b046ee03642be6ca6b72
Protesilaos Stavrou <info@protesilaos.com>
2025-11-23 05:36:49 +0200

    Bump version number to trigger GNU ELPA update
    There have been some fixes that I want to include in the package.
    Otherwise I will need to delay those until version 4.2.0, which might
    be released next year...

--- denote.el
+++ denote.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/denote
-;; Version: 4.1.1
+;; Version: 4.1.2
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
